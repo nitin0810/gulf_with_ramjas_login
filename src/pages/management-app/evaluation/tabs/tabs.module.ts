de106ea1e572d8f1b345e94d237c16b351d36bfe
Nitin Negi <nitinnegi0810@gmail.com>
2017-11-02 18:21:12 +0530

    minor bug solved by renaming the tab file of evaluation'

--- src/pages/management-app/evaluation/tabs/tabs.module.ts
+++ src/pages/management-app/evaluation/tabs/tabs.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { EvaluationTabsPageManagement } from './tab';
+import { EvaluationTabsPageManagement } from './tabs';
 
 @NgModule({
   
