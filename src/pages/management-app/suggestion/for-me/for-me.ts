7d0ee6dbc82cd27064475f08d491060a0462fd6c
akash-gupta-146 <ag.akash007@gmail.com>
2018-01-18 16:59:24 +0530

    Updated for-me filters

--- src/pages/management-app/suggestion/for-me/for-me.ts
+++ src/pages/management-app/suggestion/for-me/for-me.ts
@@ -1,15 +1,48 @@
-import { Component, ViewChild, OnDestroy } from '@angular/core';
+import { Component, ViewChild, OnDestroy, ElementRef } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
 import { ComplaintMainPage } from '../../../../custom-components/complaint-main/complaint-main';
 import { ComplaintService } from '../../../../services/complaint.service';
 
 @IonicPage()
 @Component({
-  templateUrl: './for-me.html'
+  templateUrl: './for-me.html',
+  styles: [`
+  ion-searchbar{
+    top: 45px;
+  }
+  .scroll-content{
+    padding-top: 84px !important;
+  }
+  div[subHeader]{
+    position: fixed;
+    z-index: 2;
+    transition: top 0.5s ease;
+    width: 100%;
+    top: 72px;
+  }
+  .hide-filter{
+    top: -20px !important;
+    }
+    .show-filter{
+      top: 72px;
+    }
+    ion-item[blank]{
+      height: 84px;
+    }
+  `]
 
 })
 
 export class SuggestionForMeManagement implements OnDestroy {
+
+  //variables for scroll
+  start = 0;
+  threshold = 100;
+  slideHeaderPrevious = 0;
+  ionScroll:any;
+  showHelpers:boolean;
+  hideHelpers:boolean;
+  headercontent:any;
 
   title: string = "Suggestions";
   complaintList: Array<any>;
@@ -18,13 +51,34 @@ export class SuggestionForMeManagement implements OnDestroy {
   @ViewChild(ComplaintMainPage) complaintMainPage: ComplaintMainPage;
 
   constructor(
-    private complaintService: ComplaintService
+    private complaintService: ComplaintService,
+    public myElement: ElementRef,
 
   ) {
     console.log('for me constructor called/////');
     this.complaintService.compOrSugg = "suggestion";
 
   }
+
+  ngOnInit() {
+    // Ionic scroll element
+    this.ionScroll = this.myElement.nativeElement.getElementsByClassName('scroll-content')[0];
+    // On scroll function
+    this.ionScroll.addEventListener('scroll', () => {
+        if (this.ionScroll.scrollTop - this.start > this.threshold) {
+            this.showHelpers = true;
+            this.hideHelpers = false;
+        } else {
+            this.showHelpers = false;
+            this.hideHelpers = true;
+        }
+        if (this.slideHeaderPrevious >= this.ionScroll.scrollTop - this.start) {
+            this.showHelpers = false;
+            this.hideHelpers = true;
+        }
+        this.slideHeaderPrevious = this.ionScroll.scrollTop - this.start;
+    });
+}
 
   ngOnDestroy() {
     console.log("for me destroyed.//////////");
